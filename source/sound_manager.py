cacaf7e603a7a4cc1db7797deea143a130ab179d
MarcPIster <marc.pister@epitech.eu>
2023-02-11 19:42:36 +0100

    adjust speed for sound if collection items

--- source/sound_manager.py
+++ source/sound_manager.py
@@ -15,7 +15,10 @@ class SoundManager:
         self.sound_list[sound_name] = arcade.load_sound(sound_file)
 
     def play_sound(self, sound_name):
-        player = arcade.play_sound(self.sound_list[sound_name], self.volume_sound)
+        if sound_name == "item-collect":
+            player = arcade.play_sound(self.sound_list[sound_name], self.volume_sound, speed=2)
+        else:
+            player = arcade.play_sound(self.sound_list[sound_name], self.volume_sound)
         self.player_sound[sound_name] = player
         return player
 
