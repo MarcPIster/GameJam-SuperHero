7faaffe3dd2e6f510410f9f6e6d01405e688758a
JosefineM2000 <josefine.mende@epitech.eu>
2023-02-11 11:41:47 +0100

    merge

--- main.py
+++ main.py
@@ -1,93 +1,36 @@
-"""
-Platformer Game
-"""
 import arcade
-from source.josis_test_player import Player
-from source.maps.map_manager import MapManager
+from source.menus.start_screen import StartWindow
+
 
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Smash Covid"
 
-# LAYER order:
-# 1: Platforms
-# 2: Player
-# 3: Coins
 
-class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+class SmashCovid(arcade.View):
 
     def __init__(self):
         # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        self.physics_engine = None
-        self.map = None
-        self.player = None
-        self.scene = None
-        self.map_manager = MapManager()
-        self.tile_map = None
-        self.moving_platforms = None
+        super().__init__()
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
-
-    def load_level(self, level):
-        self.moving_platforms = False
-        self.scene = arcade.Scene.from_tilemap(level)
-
-        self.scene.add_sprite_list_before("Player", "Coins")
-
-        self.player = Player()
-        self.scene.add_sprite("Player", self.player.player_sprite)
-
-        try:
-            self.player.physics_engine = arcade.PhysicsEnginePlatformer(self.player.player_sprite,
-                                                                        platforms=self.scene["Moving Platforms"],
-                                                                        walls=self.scene["Platforms"])
-            self.moving_platforms = True
-        except KeyError:
-            self.player.physics_engine = arcade.PhysicsEngineSimple(self.player.player_sprite,
-                                                                    walls=self.scene["Platforms"])
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
-        self.load_level(self.map_manager.load_level(1))
 
     def on_draw(self):
         """ Render the screen. """
-
-        self.clear()
-
-        self.scene.draw()
+        arcade.start_render()
         # Code to draw the screen goes here
 
-    def on_key_press(self, key, modifiers):
-        # TODO: remove again, just to test switching levels
-        if key == arcade.key.J:
-            self.load_level(self.map_manager.load_level(1))
-        if key == arcade.key.K:
-            self.load_level(self.map_manager.load_level(2))
-        if key == arcade.key.L:
-            self.load_level(self.map_manager.load_level(3))
-        if key == arcade.key.M:
-            self.load_level(self.map_manager.load_level(4))
 
-        self.player.on_key_press(key, modifiers)
-
-    def on_key_release(self, key, modifiers):
-        self.player.on_key_release(key, modifiers)
-
-    def on_update(self, delta_time):
-        self.player.on_update(delta_time)
-        if self.moving_platforms:
-            self.scene.update(["Moving Platforms"])
 
 
 def main():
     """ Main method """
-    window = MyGame()
-    window.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = StartWindow()
+    window.show_view(start_view)
     arcade.run()
 
 
--- source/game.py
+++ source/game.py
@@ -1,7 +1,12 @@
 import arcade
 from source.josis_test_player import Player
-from source.maps.level_one import MapOne
+from source.maps.map_manager import MapManager
 from source.menus.pause_screen import PauseManager
+
+# LAYER order:
+# 1: Platforms
+# 2: Player
+# 3: Coins
 
 class MyGame(arcade.View):
     """
@@ -13,8 +18,30 @@ class MyGame(arcade.View):
         self.physics_engine = None
         self.map = None
         self.player = None
+        self.scene = None
+        self.map_manager = MapManager()
+        self.tile_map = None
+        self.moving_platforms = None
         self.pause_manager = None
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def load_level(self, level):
+        self.moving_platforms = False
+        self.scene = arcade.Scene.from_tilemap(level)
+
+        self.scene.add_sprite_list_before("Player", "Coins")
+
+        self.player = Player()
+        self.scene.add_sprite("Player", self.player.player_sprite)
+
+        try:
+            self.player.physics_engine = arcade.PhysicsEnginePlatformer(self.player.player_sprite,
+                                                                        platforms=self.scene["Moving Platforms"],
+                                                                        walls=self.scene["Platforms"])
+            self.moving_platforms = True
+        except KeyError:
+            self.player.physics_engine = arcade.PhysicsEngineSimple(self.player.player_sprite,
+                                                                    walls=self.scene["Platforms"])
 
     def on_show_view(self):
         """ This is run once when we switch to this view """
@@ -22,20 +49,26 @@ class MyGame(arcade.View):
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
-        self.map = MapOne()
-        self.map.setup()
-        self.player = Player()
-        self.player.physics_engine = arcade.PhysicsEngineSimple(self.player.player_sprite, self.map.sprite_list)
+        self.load_level(self.map_manager.load_level(1))
         self.pause_manager = PauseManager()
 
     def on_draw(self):
         """ Render the screen. """
-        arcade.start_render()
-        self.player.player_sprite.draw()
-        self.map.sprite_list.draw()
+        self.clear()
+        self.scene.draw()
         # Code to draw the screen goes here
 
     def on_key_press(self, key, modifiers):
+        # TODO: remove again, just to test switching levels
+        if key == arcade.key.J:
+            self.load_level(self.map_manager.load_level(1))
+        if key == arcade.key.K:
+            self.load_level(self.map_manager.load_level(2))
+        if key == arcade.key.L:
+            self.load_level(self.map_manager.load_level(3))
+        if key == arcade.key.M:
+            self.load_level(self.map_manager.load_level(4))
+
         self.player.on_key_press(key, modifiers)
 
     def on_key_release(self, key, modifiers):
@@ -44,3 +77,5 @@ class MyGame(arcade.View):
 
     def on_update(self, delta_time):
         self.player.on_update(delta_time)
+        if self.moving_platforms:
+            self.scene.update(["Moving Platforms"])
