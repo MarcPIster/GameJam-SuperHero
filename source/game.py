ed2df87ebb28669526e8d12a26cfb25ae80601c6
MarcPIster <marc.pister@epitech.eu>
2023-02-11 19:56:02 +0100

    added beep for the last 10 seconds

--- source/game.py
+++ source/game.py
@@ -36,6 +36,7 @@ class MyGame(arcade.View):
 
         self.gui_camera = arcade.Camera(self.window.width, self.window.height)
         self.total_time = 0.0
+        self.countdown_time = 0
         self.timer_text = arcade.Text(
             text="00:00:00",
             start_x=self.window.width // 2,
@@ -54,6 +55,7 @@ class MyGame(arcade.View):
         self.sound_manager.add_sound(f'dead', f'./assets/sounds/diesound.wav')
         self.sound_manager.add_sound(f'coin-collect', f'./assets/sounds/coincollect.wav')
         self.sound_manager.add_sound(f'item-collect', f'./assets/sounds/itemcollect.wav')
+        self.sound_manager.add_sound(f'beep', f'./assets/sounds/beep.wav')
 
 
     def setup(self, level=1):
@@ -179,13 +181,18 @@ class MyGame(arcade.View):
 
         # update timer
         self.total_time += delta_time
-        time_passed = 120 - self.total_time
+        time_passed = 10 - self.total_time
         minutes = int(time_passed) // 60
         seconds = int(time_passed) % 60
         seconds_100s = int((time_passed - (minutes * 60) - seconds) * 100)
         self.timer_text.text = f"{minutes:02d}:{seconds:02d}:{seconds_100s:02d}"
         if time_passed <= 0:
             self.end_game()
+        if time_passed <= 10:
+            self.countdown_time += delta_time
+            if self.countdown_time >= 1:
+                self.sound_manager.play_sound('beep')
+                self.countdown_time = 0
 
         # check item collision
         player_collision_list = arcade.check_for_collision_with_lists(self.player, [
