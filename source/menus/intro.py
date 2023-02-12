9217cc59250aeb229150fd8d25a0f7887138db30
MarcPIster <marc.pister@epitech.eu>
2023-02-12 21:57:20 +0100

    change start position of bat

--- source/menus/intro.py
+++ source/menus/intro.py
@@ -1,22 +1,20 @@
 import arcade
-import arcade.gui
 import random
 
 class Intro:
         def __init__(self):
             self.timer = 0
             self.player = 1
-            self.manager = arcade.gui.UIManager()
-            self.manager.enable()
+
+
             self.bat = arcade.Sprite("./assets/bat/idle/idle0.png", 2)
-            self.bat.center_x = 100
+            self.bat.center_x = 400
             self.bat.center_y = 800
             self.bat_movement = [0, 0]
             self.physics_engine = arcade.PhysicsEnginePlatformer(self.bat, walls=[arcade.Sprite("./assets/player/walk/walk0.png", 2)])
 
 
         def on_draw(self):
-            self.manager.draw()
             self.bat.draw()
 
         def on_update(self, delta_time):
@@ -24,7 +22,7 @@ class Intro:
             self.bat.update_animation()
             if self.timer > 0.5:
                 self.timer = 0
-                self.bat_movement = [random.randint(-5, 5), random.randint(-5, 5)]
+                self.bat_movement = [random.randint(-5, 5), random.randint(-5, 4)]
 
             if self.bat.left < 0:
                 self.bat.left = 0
